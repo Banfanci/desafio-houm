acefb23d64f91463750ed1834254229de43c88d6
Banfanci <bflobos@uc.cl>
2022-03-02 01:19:01 -0300

    hero card created

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -4,6 +4,7 @@ import { ChakraProvider, extendTheme } from '@chakra-ui/react'
 const theme = extendTheme({
   shadows: {
     md: '0 4px 4px 0 rgba(219, 219, 219, 0.25)',
+    lg: '0 4px 4px 0 rgba(219, 219, 219, 0.5)',
     outline: '0 0 0 2px rgba(255, 69, 43, 0.4)',
   },
   colors: {
@@ -14,6 +15,10 @@ const theme = extendTheme({
     houmBlue: {
       500: '#263238',
       600: '#607D8B',
+    },
+    houmLetters: {
+      title: '#212121',
+      subTitle: '#607D8B',
     },
   },
   components: {
--- pages/index.jsx
+++ pages/index.jsx
@@ -8,27 +8,24 @@ import {
   InputGroup,
   Input,
   InputRightElement,
+  SimpleGrid,
 } from '@chakra-ui/react'
+
+import HeroCard from '../components/HeroCard/HeroCard'
 
 export default function Home() {
   return (
     <>
-      <Box boxShadow="md" rounded="md" h="4vw">
-        <Image
-          src="/assets/houmLogo.svg"
-          h="3vw"
-          margin="1vw 2vw"
-          alt="houmLogo"
-        />
+      <Box boxShadow="md" rounded="md" h="4em">
+        <Image src="/assets/houmLogo.svg" h="3em" m="5" alt="houmLogo" />
       </Box>
-      <Center margin="5vw">
-        <InputGroup size="md" width="40vw">
+      <Center margin="2em">
+        <InputGroup size="md" w={{ base: '60vw', lg: '40vw' }}>
           <Input
-            pr="4.5rem"
             placeholder="Busca por nombre"
             focusBorderColor="houmOrange.100"
           />
-          <InputRightElement width="4.5rem">
+          <InputRightElement w={{ base: '10vw', lg: '4.5vw' }}>
             <IconButton
               icon={<SearchIcon />}
               isRound
@@ -43,11 +40,17 @@ export default function Home() {
           colorScheme="houmBlue"
           size="md"
           borderRadius="30px"
-          w="8vw"
-          ml="2vw"
+          w={{ base: '18vw', md: '14vw', lg: '10vw' }}
+          m="4"
+          p="4"
         >
           Filtrar
         </Button>
+      </Center>
+      <Center>
+        <SimpleGrid columns={{ base: 1, md: 2, xl: 3 }}>
+          <HeroCard></HeroCard>
+        </SimpleGrid>
       </Center>
     </>
   )
