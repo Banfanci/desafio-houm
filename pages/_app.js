b4c87700f45adb422344ff9d64d9b47c1123a932
Banfanci <bflobos@uc.cl>
2022-03-01 22:57:14 -0300

    added search box and filter button

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,26 @@ import { ChakraProvider, extendTheme } from '@chakra-ui/react'
 const theme = extendTheme({
   shadows: {
     md: '0 4px 4px 0 rgba(219, 219, 219, 0.25)',
+    outline: '0 0 0 2px rgba(255, 69, 43, 0.4)',
+  },
+  colors: {
+    houmOrange: {
+      100: 'rgba(255, 69, 43, 0.4)',
+      600: '#FF452B',
+    },
+    houmBlue: {
+      500: '#263238',
+      600: '#607D8B',
+    },
+  },
+  components: {
+    Button: {
+      baseStyle: {
+        _focus: {
+          boxShadow: 'none',
+        },
+      },
+    },
   },
 })
 
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,54 @@
-import { Box, Image } from '@chakra-ui/react'
+import { SearchIcon, SettingsIcon } from '@chakra-ui/icons'
+import {
+  Box,
+  Button,
+  IconButton,
+  Center,
+  Image,
+  InputGroup,
+  Input,
+  InputRightElement,
+} from '@chakra-ui/react'
 
 export default function Home() {
   return (
-    <Box boxShadow="md" rounded="md" h="4vw">
-      <Image
-        src="/assets/houmLogo.svg"
-        h="3vw"
-        margin="1vw 2vw"
-        alt="houmLogo"
-      />
-    </Box>
+    <>
+      <Box boxShadow="md" rounded="md" h="4vw">
+        <Image
+          src="/assets/houmLogo.svg"
+          h="3vw"
+          margin="1vw 2vw"
+          alt="houmLogo"
+        />
+      </Box>
+      <Center margin="5vw">
+        <InputGroup size="md" width="40vw">
+          <Input
+            pr="4.5rem"
+            placeholder="Busca por nombre"
+            focusBorderColor="houmOrange.100"
+          />
+          <InputRightElement width="4.5rem">
+            <IconButton
+              icon={<SearchIcon />}
+              isRound
+              size="sm"
+              variant="outline"
+              colorScheme="houmOrange"
+            />
+          </InputRightElement>
+        </InputGroup>
+        <Button
+          leftIcon={<SettingsIcon />}
+          colorScheme="houmBlue"
+          size="md"
+          borderRadius="30px"
+          w="8vw"
+          ml="2vw"
+        >
+          Filtrar
+        </Button>
+      </Center>
+    </>
   )
 }
