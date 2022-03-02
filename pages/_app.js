6023988cc8c84d2c6c3764be9275d45e6ccd502c
Banfanci <bflobos@uc.cl>
2022-03-01 21:21:06 -0300

    houm logo added

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,15 @@
 import '../styles/globals.css'
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
+
+const theme = extendTheme({
+  shadows: {
+    md: '0 4px 4px 0 rgba(219, 219, 219, 0.25)',
+  },
+})
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Component {...pageProps} />
     </ChakraProvider>
   )
